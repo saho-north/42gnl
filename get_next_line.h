bd5e10874dd6650154746d4c78b44950dec994f9
Saho Kitahara <sakitaha@c1r25s8.42tokyo.jp>
2023-06-10 05:58:26 +0900

    modified

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 23:04:58 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/06/08 05:57:07 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/06/10 05:46:00 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <limits.h>
 # include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -22,10 +23,9 @@
 #  define BUFFER_SIZE 32
 # endif
 
-char	*get_current_line(char **buffered_text);
 char	*extract_next_line(char **buffered_text);
 char	*strjoin_plus(char *buffered_text, char *read_buffer);
-char	*read_from_file(int fd, char **buffered_text);
+bool	read_from_file(int fd, char **buffered_text);
 char	*get_next_line(int fd);
 char	*ft_calloc(size_t count, size_t size);
 char	*ft_strchr(const char *s, int c);
